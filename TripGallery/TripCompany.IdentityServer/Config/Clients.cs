0db120b9a20428ee4ae4d6297d2dcdbd3114f101
Amilton Costa <amilton.jbc@gmail.com>
2016-03-24 07:54:41 -0300

    Commit Finalização dos Trabalhos 24/03/2016

---
+++ TripGallery/TripCompany.IdentityServer/Config/Clients.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripCompany.IdentityServer.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "tripgalleryclientcredentials",
+                    ClientName = "Trip Gallery (Client Credentials)",
+                    Flow = Flows.ClientCredentials,
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret(TripGallery.Constants.TripGalleryClientSecret.Sha256())
+                    },                    
+                    AllowAccessToAllScopes = true //Permitindo acesso para todos os scopos
+                    //Isso pode ser útil quando você tem vários clientes e você só quer permitir
+                    //que um conjunto específico de escopos seja liberado para um cliente específico.
+
+                }
+            };
+        }
+
+
+    }
+}
---
+++ TripGallery/TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
@@ -0,0 +1,60 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TripGallery.MVCClient.Helpers
+{
+    public static class TripGalleryHttpClient
+    {
+
+        public static HttpClient GetClient()
+        { 
+            HttpClient client = new HttpClient();
+
+            //Client Credendials FLOW
+            var accessToken = RequestAccessTokenClientCredentials();
+            client.SetBearerToken(accessToken);
+           
+            client.BaseAddress = new Uri(Constants.TripGalleryAPI);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private static string RequestAccessTokenClientCredentials()
+        {
+            //Criar um Token Cliente
+            var tokenClient = new TokenClient(
+                TripGallery.Constants.TripGallerySTSTokenEndpoint,
+                "tripgalleryclientcredentials",
+                TripGallery.Constants.TripGalleryClientSecret);
+
+
+            //ask for a token, containing the galleymanagement scope
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("gallerymanagement").Result;
+
+            //decode & write out the token, so we can see what's is iten
+            TokenHelper.DecodeAndWrite(tokenResponse.AccessToken);
+
+
+            //We save the token in a cookie for use later on
+            HttpContext.Current.Response.Cookies["TripGalleryCookie"]["acccess_token"] = tokenResponse.AccessToken;
+
+
+            return tokenResponse.AccessToken;
+
+
+        }
+ 
+    }
+}
